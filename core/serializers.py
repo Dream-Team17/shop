80f6134285160c16a03fc9e5321111ffa7af3897
Dream-Team17 <scout31217@gmail.com>
2023-03-14 08:02:45 +0600

    changed serializers

--- core/serializers.py
+++ core/serializers.py
@@ -2,20 +2,6 @@ from rest_framework import serializers
 from .models import Product, Category, Subcategory, Vacant
 
 
-class ProductSerializer(serializers.ModelSerializer):
-    price = serializers.FloatField(min_value=1)
-    discount = serializers.FloatField(min_value=0)
-    discount_price = serializers.FloatField(min_value=0)
-    weight_volume = serializers.FloatField(min_value=1)
-
-    class Meta:
-        model = Product
-        fields = (
-            'id', 'name', 'product_slug', 'image', 'description', 'price', 'categories',
-            'subcategories', 'available', 'created_date', 'updated_date', 'is_discount', 'is_new', 'discount_price',
-            'discount', 'valid_time', 'weight_volume', 'product_code')
-
-
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
@@ -23,9 +9,26 @@ class CategorySerializer(serializers.ModelSerializer):
 
 
 class SubcategorySerializer(serializers.ModelSerializer):
+    categories = CategorySerializer(many=False)
     class Meta:
         model = Subcategory
         fields = ('id', 'name', 'categories', 'subcategory_slug')
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    price = serializers.FloatField(min_value=1)
+    discount = serializers.FloatField(min_value=0)
+    discount_price = serializers.FloatField(min_value=0)
+    weight_volume = serializers.FloatField(min_value=1)
+    categories = CategorySerializer(many=False)
+    subcategories = SubcategorySerializer(many=False)
+
+    class Meta:
+        model = Product
+        fields = (
+            'id', 'name', 'product_slug', 'image', 'description', 'price', 'categories',
+            'subcategories', 'available', 'created_date', 'updated_date', 'is_discount', 'is_new', 'discount_price',
+            'discount', 'valid_time', 'weight_volume', 'product_code')
 
 
 class NewProductSerializer(serializers.ModelSerializer):
